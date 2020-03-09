5f7542b1e6f61f91729c13e7b280ba5d3cbd07e1
Joon Woo Park <canejune@gmail.com>
2020-03-09 16:47:54 -0700

    Done 27

--- 27.cpp
+++ 27.cpp
@@ -7,11 +7,23 @@ using namespace std;
 
 int main(void)
 {
-    int n;
+    int n, i, j, tmp;
     cin >> n;
 
-    vector<int> a(n, 0);
+    vector<int> a(n + 1, 0);
 
-    
-
+    for (i = 2; i <= n; i++) {
+        tmp = i;
+        for (j = 2; j <= tmp;) {
+            if (tmp % j == 0) {
+                a[j]++;
+                tmp /= j;
+            }
+            else j++;
+        }
+    }
+    for (i = 2; i <= n; i++) {
+        if (a[i]) cout << a[i] << " ";
+    }
+    cout << endl;
 }
